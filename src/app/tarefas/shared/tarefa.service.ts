ecc0bca828ab0f55a2f98eb95791d7af57737582
Juliano Sarnes Longo <longo.juliano@turingbits.com.br>
2021-12-16 22:31:00 -0300

    Implementação dos componentes lista e cadastrar tarefa, implementação do serviço ListarTarefa.

---
+++ src/app/tarefas/shared/tarefa.service.ts
@@ -0,0 +1,53 @@
+import { Injectable } from '@angular/core';
+import { Tarefa } from './';
+
+@Injectable()
+export class TarefaService {
+
+  constructor() { }
+
+  listarTodos(): Tarefa[] {
+    const tarefas = localStorage['tarefas'];
+    return tarefas ? JSON.parse(tarefas) : [];
+  }
+
+  cadastrar(tarefa: Tarefa): void {
+    const tarefas = this.listarTodos();
+    tarefa.id = new Date().getTime();
+    tarefas.push(tarefa);
+    localStorage['tarefas'] = JSON.stringify(tarefas); //deserealiza o objeto ou seja converte em uma string.
+  }
+
+  buscarPorId(id: number): Tarefa {
+    const tarefas: Tarefa[] = this.listarTodos(); 
+    return tarefas.find(tarefa => tarefa.id === id);
+  }
+
+  atualizarTarefa(tarefa: Tarefa) : void {
+    const tarefas: Tarefa[] = this.listarTodos();
+    tarefas.forEach((obj, index, objs) => {
+      if (tarefa.id === obj.id) {
+        objs[index] = tarefa;
+      }
+    });
+    localStorage['tarefas'] = JSON.stringify(tarefas);
+  }
+
+  remover(id: number): void {
+    let tarefas: Tarefa[] = this.listarTodos();
+    //retorna todas as tarefas que sao diferentes do id que eu quero remover.
+    tarefas = tarefas.filter(tarefa => tarefa.id !== id);
+    localStorage['tarefas'] = JSON.stringify(tarefas);
+  }
+
+  alterarStatus(id: number) : void {
+    const tarefas: Tarefa[] = this.listarTodos();
+    tarefas.forEach((obj, index, objs) => {
+      if (id === obj.id) {
+        objs[index].concluida = !obj.concluida;
+      }
+    });
+    localStorage['tarefas'] = JSON.stringify(tarefas);
+  }
+
+}
